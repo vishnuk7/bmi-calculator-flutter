f1390c3f8336bcaa1f8ae2ff7d241665520c46f5
viu <viu@vius-Mac.local>
2020-07-11 06:01:27 -0700

    custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        accentColor: Colors.amber,
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.white),
+        ),
+      ),
       home: InputPage(),
     );
   }
@@ -23,12 +31,56 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: CustomCard(
+                  cardColor: Color(0xFF1D1E33),
+                )),
+                Expanded(
+                  child: CustomCard(
+                    cardColor: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+              child: CustomCard(
+            cardColor: Color(0xFF1D1E33),
+          )),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                  child: CustomCard(
+                cardColor: Color(0xFF1D1E33),
+              )),
+              Expanded(
+                  child: CustomCard(
+                cardColor: Color(0xFF1D1E33),
+              ))
+            ],
+          ))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final Color cardColor;
+  CustomCard({@required this.cardColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: cardColor, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
